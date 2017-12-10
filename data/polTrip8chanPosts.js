5263e5e5d8a9410a836c3569bf6268ad984db07e
QCodefag <34404349+QCodefag@users.noreply.github.com>
2017-12-10 11:33:40 +0100

    wsBcBAABCAAQBQJaLQ2ECRBK7hj4Ov3rIwAAdHIIAEuIL5MqJqQPBdsY1aly+mET  CSxgdQ8LmA1Mwl3zY/TZ4UtcNYnlw3/qXzw7yYomLkQhhWmPFVfU2ylVrY4cmnsu  90Jz9Nc/AkW0H/fxMQWTVqqaM6Nf+9HiJuzV17f89c/dgi71wGo9hJeAZck0cdus  jduekkBbDU9VdGMsiYjdTnNeWPnWNzV97fPtpuePEgf3P/tCAe5+iVXDtoKcuWhv  GuSRUUdlZAjzRC3LU2xseqa/Yu00qa6jx59Yw8YaNo7lMW/frGP/TBwpUeMG6Oo7  at7o8y0+qJM8zTc2Eq8yFj03OnHQlCYHoL4FOyf2jFXT52ruLeODSv1ffiGtYIo=  =6DJU  -----END PGP SIGNATURE-----
    Create polTrip8chanPosts.js

---
+++ data/polTrip8chanPosts.js
@@ -0,0 +1,16 @@
+const polTrip8chanPosts = [
+  {
+    "postId": 10956085,
+    "text": "Test\nTest\n4Chan infiltrated.\nFuture posts will be relayed here.\nQ\n",
+    "name": "Q ",
+    "trip": "!ITPb.qbhqo",
+    "timestamp": 1511636085,
+  },
+  {
+    "postId": 10956374,
+    "text": "Flash Dir_\nStart_code_activated/instruction\nLOG1_^67FVc\n_4ch_n\n_8ch_y\n_Conf_y_[8]_8bCon\nKey secured.\nQ\n",
+    "name": "Q ",
+    "trip": "!ITPb.qbhqo",
+    "timestamp": 1511641718
+  }
+]
